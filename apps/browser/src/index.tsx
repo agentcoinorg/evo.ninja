0d85d6b9fa2e725f78598f7b733cf92df5adc2ea
nerfZael <bogunovij@gmail.com>
2023-08-21 12:01:05 +0200

    added buffer polyfill

--- apps/browser/src/index.tsx
+++ apps/browser/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+window.Buffer = window.Buffer || require("buffer").Buffer;
 import App from './App';
 
 const root = ReactDOM.createRoot(
