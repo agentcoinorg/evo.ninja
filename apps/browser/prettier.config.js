d52fe692d7803672193f98813e99f9a672902c85
Pileks <jure@amber-it.co>
2023-09-12 10:25:54 +0200

    add prettier (no auto-linting), minor refactor of CSS

---
+++ apps/browser/prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "auto",
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+module.exports = config;
