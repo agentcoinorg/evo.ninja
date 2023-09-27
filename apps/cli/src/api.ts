482b3586e621eb180e50868d299f584980c32fbb
Cesar <cesarbrazon10@gmail.com>
2023-09-27 11:53:44 +0200

    chore: remove scripts per run in api process

--- apps/cli/src/api.ts
+++ apps/cli/src/api.ts
@@ -8,6 +8,24 @@ import Agent, {
   StepResult,
   TaskInput,
 } from "forked-agent-protocol";
+import { exec } from "child_process";
+
+function execPromise(command: string) {
+  return new Promise(function (resolve, reject) {
+    exec(
+      command,
+      { cwd: path.join(__dirname, "../../../scripts") },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        console.log(stdout);
+        resolve(stdout.trim());
+      }
+    );
+  });
+}
 
 async function taskHandler(
   id: string,
@@ -23,13 +41,16 @@ async function taskHandler(
     taskId: id,
     debug: true,
   });
-
+  app.logger.info("\n////////////////////////////////////////////");
+  app.logger.info(`Trying to achieve goal: ${input}\nTask with ID: ${id}`);
   app.debugLog?.goalStart(input);
   let iterator = app.evo.run(input);
+
   async function stepHandler(stepInput: StepInput | null): Promise<StepResult> {
     app.debugLog?.stepStart();
     const response = await iterator.next(stepInput);
     app.debugLog?.stepEnd();
+    app.logger.info(`Running step....`);
     const outputTitle =
       response.value && "title" in response.value
         ? response.value.title
@@ -46,10 +67,19 @@ async function taskHandler(
 
     if (response.done) {
       if (!response.value.ok) {
+        app.logger.error(response.value.error ?? "Unknown error");
         app.debugLog?.stepError(response.value.error ?? "Unknown error");
       } else {
+        app.logger.info(JSON.stringify(response.value.value) as any);
         app.debugLog?.stepLog(response.value.value as any);
       }
+      app.logger.info("Task is done - Removing generated scripts...");
+      await execPromise("git clean -fd");
+      app.logger.info("////////////////////////////////////////////\n");
+    }
+
+    if (outputMessage !== "No Message") {
+      app.logger.info(JSON.stringify(outputMessage));
     }
     return {
       is_last: response.done,
