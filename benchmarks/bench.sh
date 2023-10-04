9580f347194c52be639dc5a6d155f5ceb057b1e4
nerfZael <bogunovij@gmail.com>
2023-10-04 19:20:09 +0200

    passing number of jobs to benc script

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -6,5 +6,8 @@ if ! command -v parallel >/dev/null; then
     exit 1
 fi
 
+# Get the number of jobs from the command line argument (default to 1 if not provided)
+NUM_JOBS="${1:-1}"
+
 # Run each command in parallel and log its output to a corresponding log file
-parallel --jobs 5 --results logs/ "{}" :::: commands.txt
+parallel --jobs "$NUM_JOBS" --results logs/ "{}" :::: commands.txt
