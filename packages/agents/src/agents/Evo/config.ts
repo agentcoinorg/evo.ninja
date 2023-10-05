86b0366ffdbea206888804b68fe36f986798c6db
dOrgJelli <jelli@dorg.tech>
2023-10-05 19:50:29 +0200

    fix: improve evo termination by moving to functions

--- packages/agents/src/agents/Evo/config.ts
+++ packages/agents/src/agents/Evo/config.ts
@@ -4,6 +4,8 @@ import { executeScriptFunction } from "./functions/executeScript";
 import { findScriptFunction } from "./functions/findScript";
 import { readVariableFunction } from "./functions/readVariable";
 import { delegateSubAgent } from "./functions/delegateSubAgent";
+import { createOnGoalAchievedFunction, createOnGoalFailedFunction } from "../../subagents/utils";
+import { buildScriptExecutor } from "../../subagents/buildScriptExecutor";
 
 export interface EvoRunArgs {
   goal: string
@@ -12,6 +14,9 @@ export interface EvoRunArgs {
 export interface EvoContext extends SubAgentContext {
   globals: Record<string, string>;
 }
+
+const onGoalAchievedFunction = createOnGoalAchievedFunction("Evo");
+const onGoalFailedFunction = createOnGoalFailedFunction("Evo");
 
 export const EVO_AGENT_CONFIG = (subagents?: SubAgentConfig[]): AgentBaseConfig<EvoRunArgs, EvoContext> => {
   const config: AgentBaseConfig<EvoRunArgs, EvoContext> = {
@@ -23,7 +28,7 @@ export const EVO_AGENT_CONFIG = (subagents?: SubAgentConfig[]): AgentBaseConfig<
 I am an expert assistant designed to achieve users' goals. I prioritize using available subagents and scripts efficiently and always give preference to existing resources.
 
 Core Principle:
-I always attempt to adapt and use existing subagents and scripts to achieve the goal. I can create new scripts as a last-resort if existing resources do not suffice. Once the user's goal has been achieved, I will call the script agent.onGoalAchieved.
+I always attempt to adapt and use existing subagents and scripts to achieve the goal. I can create new scripts as a last-resort if existing resources do not suffice. Once the user's goal has been achieved, I will call the agent_onGoalAchieved function.
 
 Functionalities:
 delegate{SubAgent}: I delegate tasks to {SubAgent}s when they have an expertise that fits the current task.
@@ -31,17 +36,15 @@ findScript: I actively search and identify scripts that can be repurposed or com
 createScript: I ONLY resort to creating a specific script when I'm certain that no existing script can be modified or combined to solve the problem. I never create overly general or vague scripts.
 executeScript: I run scripts using TypeScript syntax. I store outcomes in global variables with the 'result' argument and double-check the script's output to ensure it's correct.
 readVariable: I can access the JSON preview of a global variable. I use this to read through partial outputs of scripts, if they are too large, to find the desired information.
-
-Feedback Scripts:
-agent.onGoalAchieved: I signal when a task has been successfully completed.
-agent.onGoalFailed: I alert when a task cannot be accomplished.
+agent_onGoalAchieved: I call this when a task has been successfully completed.
+agent_onGoalFailed: I call this when a task cannot be accomplished.
 
 Decision-making Process:
 I first evaluate the goal and see if it can be achieved without using subagents or scripts.
 Then, I see if I can delegate the task to a subagent if they have a relevant expertise.
 If no relevant subagents exist, then I use findScript to search for any script that can be adapted, combined, or slightly modified to fit the task. It's vital for me to exhaust all possibilities here.
 Only when all existing script avenues have been explored and found lacking, I cautiously consider createScript. I ensure any new script I create is specific, actionable, and not general.
-If a goal has been achieved or failed, I will use the termination scripts to exit the agent loop.
+If a goal has been achieved or failed, I will call agent_onGoalAchieved or agent_onGoalFailed.
 
 User Engagement:
 I do not communicate with the user. I execute goals to the best of my abilities without any user input. I terminate when a goal has been achieved or failed.`
@@ -57,6 +60,26 @@ I do not communicate with the user. I execute goals to the best of my abilities 
       executeScriptFunction,
       findScriptFunction,
       readVariableFunction,
+      {
+        definition: onGoalAchievedFunction,
+        buildExecutor: (context: EvoContext) =>
+          buildScriptExecutor({
+            context,
+            scriptName: "agent.onGoalAchieved",
+            onSuccess: onGoalAchievedFunction.success,
+            onFailure: onGoalAchievedFunction.failure
+          })
+      },
+      {
+        definition: onGoalFailedFunction,
+        buildExecutor: (context: EvoContext) =>
+          buildScriptExecutor({
+            context,
+            scriptName: "agent.onGoalFailed",
+            onSuccess: onGoalFailedFunction.success,
+            onFailure: onGoalFailedFunction.failure
+          })
+      }
     ],
     shouldTerminate: (functionCalled) => {
       return [
