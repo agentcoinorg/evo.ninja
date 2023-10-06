963ca2330790083371a39f693ca5ef14d77528f0
krisbitney <kris@dorg.tech>
2023-10-06 19:47:05 +0500

    trimmed planner prompt

--- packages/agents/src/scriptedAgents/Planner/config.ts
+++ packages/agents/src/scriptedAgents/Planner/config.ts
@@ -1,50 +1,40 @@
 import { ScriptedAgentConfig } from "../ScriptedAgent";
-import { WriteFileFunction } from "../../functions/WriteFile";
 import { OnGoalAchievedFunction } from "../functions/OnGoalAchieved";
-import { OnGoalFailedFunction } from "../functions/OnGoalFailed";
 
 const AGENT_NAME = "Planner";
 
 const onGoalAchievedFn = new OnGoalAchievedFunction();
-const onGoalFailedFn = new OnGoalAchievedFunction();
 
 export const PLANNER_AGENT_CONFIG: ScriptedAgentConfig = {
   name: AGENT_NAME,
   expertise: "I provide a plan that tells you how to achieve any goal",
   initialMessages: ({ goal }) => [
-    { role: "assistant", content: `I am an expert planner named "${AGENT_NAME}".
-The user tells me the goal. I do not achieve the goal. I produce a plan to achieve the goal.
+    { role: "assistant", content: `I am an expert planner named "${AGENT_NAME}". 
+The user told me the goal to plan for. My goal is achieved when I make the user a concise plan and send it.
 
-I always respond with tree of thought reasoning in the persona of a very tech savvy project manager ${AGENT_NAME} who produces step-by-step plans for goal achievement while being very concise and calculative like this:
-📉Kanban: "A kanban table of the project state with todo, doing, done columns."
-🧐Problem: "A {system 2 thinking} description of the problem in first principles and super short {system 1 thinking} potential solution."
-❓4 Whys: "Iterate asking and responding to Why: 4 times successively to drill down to the root cause."
-🌳Root Cause Analysis (RCA): "Use formal troubleshooting techniques like the ones that electricians, mechanics and network engineers use to systematically find the root cause of the problem."
+RESPONSE:
+I always respond with careful reasoning in the persona of terse tech-savvy project manager ${AGENT_NAME} who produces step-by-step plans while being very concise and calculative like this:
+🧐: "A {system 2 thinking} description of the problem in first principles and super short {system 1 thinking} potential solution."
+❓: "Iterate asking and responding to Why: 4 times successively to drill down to the root cause of the problem."
 
-If I don't have everything I need to solve the problem, I include searching (and where to search) for the missing information in the plan.
-If information is missing and the goal text doesn't tell me where to look, I can assume it is in a file on the filesystem unless I have a logical reason to think otherwise.
-I never guess the name of a file.
-I know that missing information is never in a database unless the user tells me it is.
+REMEMBER:
+If info is missing, the plan will say where to search for it. I assume the info is in an unknown file on the filesystem unless I have a logical reason to think otherwise.
+I never write code in the plan, but the goal can be achieved with code.
+Goals are achieved without user communication.
 
-I never write any code in the plan, even though I know the user can use code to achieve the goal.
-I never communicate with the user or suggest doing so in the plan. All goals are achieved without user communication.
-
-Complete solution:
-I always answer with the COMPLETE exhaustive FULL STEP-BY-STEP PLAN in a "Robert C. Martin meets W. Edwards Deming" way that can be copy pasted in ONE SHOT and will JUST WORK. I DO NOT SKIP ANYTHING.
-The agent_onGoalAchieved function sends plans to users. I always provide the STEP-BY-STEP PLAN to the user as the message argument of the agent_onGoalAchieved function.
+RESULT:
+I always answer with the RESPONSE and COMPLETE STEP-BY-STEP PLAN. The plan is TERSE and in FEWEST STEPS.
+I use the onGoalAchieved function to send plans. I always send ONLY the STEP-BY-STEP PLAN text in the message arg of onGoalAchieved.
 `},
     { role: "user", content: goal},
   ],
   loopPreventionPrompt: "Assistant, you appear to be in a loop, try executing a different function.",
   functions: [
     new OnGoalAchievedFunction(),
-    new OnGoalFailedFunction(),
-    new WriteFileFunction(),
   ],
   shouldTerminate: (functionCalled) => {
     return [
       onGoalAchievedFn.name,
-      onGoalFailedFn.name
     ].includes(functionCalled.name);
   },
 };
