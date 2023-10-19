55b844c66827c69c08e37f8a49b91ec4f210899c
nerfZael <bogunovij@gmail.com>
2023-10-19 13:42:51 +0200

    removed unused factory

--- packages/agents/src/scriptedAgents/ScriptedAgentFactory.ts
+++
@@ -1,4 +0,0 @@
-import { AgentBaseContext } from "../AgentBase";
-import { ScriptedAgent } from "./ScriptedAgent";
-
-export type ScriptedAgentFactory = (context: AgentBaseContext) => ScriptedAgent;
