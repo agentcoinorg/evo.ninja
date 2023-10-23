5c86d27502a2e0596bd043706a1f8e87c466dd9a
nerfZael <bogunovij@gmail.com>
2023-10-23 18:17:34 +0200

    refactor

--- packages/agents/src/agents/Chameleon/index.ts
+++ packages/agents/src/agents/Chameleon/index.ts
@@ -1,6 +1,5 @@
 import {
   AgentFunction,
-  Chat,
   ChatLogs,
   ChatMessage,
   FunctionDefinition,
@@ -52,7 +51,7 @@ export class ChameleonAgent extends NewAgent<GoalRunArgs> {
     const lastMessage = messages.slice(-1)[0];
 
     if (isLargeMsg(lastMessage)) {
-      await shortenMessage(lastMessage, this.lastQuery!, this.context, this.askLlm.bind(this));
+      await this.shortenMessage(lastMessage, this.lastQuery!);
     }
     
     const query = await this.predictBestNextStep(messages);
@@ -60,7 +59,7 @@ export class ChameleonAgent extends NewAgent<GoalRunArgs> {
     this.lastQuery = query;
     console.log("Query: ", query);
 
-    await shortenLargeMessages(query, chat, this.context, this.askLlm.bind(this));
+    await this.shortenLargeMessages(query);
 
     const [agent, agentFunctions, persona, allFunctions] = await findBestAgent(query, this.context);
 
@@ -80,7 +79,7 @@ export class ChameleonAgent extends NewAgent<GoalRunArgs> {
     }
   }
 
-  async predictBestNextStep(messages: ChatMessage[]): Promise<string> {
+  private async predictBestNextStep(messages: ChatMessage[]): Promise<string> {
     //TODO: Keep persona in chat logs when doing this
     return await this.askLlm(
       new Prompt()
@@ -94,6 +93,78 @@ export class ChameleonAgent extends NewAgent<GoalRunArgs> {
         )
     );
   };
+
+  private async shortenLargeMessages(query: string): Promise<void> {
+    for(let i = 2; i < this.context.chat.chatLogs.messages.length ; i++) {
+      const message = this.context.chat.chatLogs.messages[i];
+      if (isLargeMsg(message)) {
+        await this.shortenMessage(message, query);
+      }
+    }
+  }
+
+  private async shortenMessage(message: ChatMessage, query: string): Promise<void> {
+    message.content = await this.advancedFilterText(message.content ?? "", query);
+  }
+
+  // private async filterText(text: string, query: string, context: AgentContext): Promise<string> {
+  //   const chunks = TextChunker.parentDocRetrieval(text, {
+  //     parentChunker: (text: string) => TextChunker.sentences(text),
+  //     childChunker: (parentText: string) =>
+  //       TextChunker.fixedCharacterLength(parentText, { chunkLength: 100, overlap: 15 })
+  //   });
+
+  //   const result = await Rag.standard(chunks, context)
+  //     .selector(x => x.doc)
+  //     .limit(12)
+  //     .sortByIndex()
+  //     .onlyUnique()
+  //     .query(query);
+
+  //   return previewChunks(
+  //     result.map(x => x.metadata.parent),
+  //     2000
+  //   );
+  // }
+
+  private async advancedFilterText(text: string, query: string): Promise<string> {
+    const chunks = TextChunker.parentDocRetrieval(text, {
+      parentChunker: (text: string) => TextChunker.sentences(text),
+      childChunker: (parentText: string) =>
+        TextChunker.fixedCharacterLength(parentText, { chunkLength: 100, overlap: 15 })
+    });
+
+    const result = await Rag.standard(chunks, this.context)
+      .selector(x => x.doc)
+      .limit(48)
+      .sortByIndex()
+      .onlyUnique()
+      .query(query);
+
+    const bigPreview = previewChunks(
+      result.map(x => x.metadata.parent),
+      8000
+    );
+
+    const prompt = new Prompt()
+      .block(bigPreview)
+      .line(`Goal:`)
+      .line(x => x.block(query))
+      .line(`
+        Consider the above text in respect to the desired goal.
+        Rewrite the text to better achieve the goal.
+        Filter out unecessary information. Do not add new information.
+        IMPORTANT: Respond only with the new text!`
+      );
+
+    console.log(prompt.toString());
+
+    const filteredText = await this.askLlm(prompt);
+
+    console.log("filteredText", text);
+
+    return filteredText;
+  }
 }
 
 const insertPersonaAsFirstMsg = (persona: string, logs: ChatLogs, tokenizer: Tokenizer): ChatLogs => {
@@ -112,78 +183,6 @@ const insertPersonaAsFirstMsg = (persona: string, logs: ChatLogs, tokenizer: Tok
 const isLargeMsg = (message: ChatMessage): boolean => {
   return !!message.content && message.content.length > 2000;
 }
-
-const shortenLargeMessages = async (query: string, chat: Chat, context: AgentContext, askLlm: (query: string | Prompt) => Promise<string>): Promise<void> => {
-  for(let i = 2; i < chat.chatLogs.messages.length ; i++) {
-    const message = chat.chatLogs.messages[i];
-    if (isLargeMsg(message)) {
-      await shortenMessage(message, query, context, askLlm);
-    }
-  }
-};
-
-const shortenMessage = async (message: ChatMessage, query: string, context: AgentContext, askLlm: (query: string | Prompt) => Promise<string>): Promise<void> => {
-  await advancedFilterMessageContent(message, query, context, askLlm);
-};
-
-// const filterMessageContent = async (message: ChatMessage, query: string, context: AgentContext, askLlm: (query: string | Prompt) => Promise<string>): Promise<void> => {
-//   const chunks = TextChunker.parentDocRetrieval(message.content ?? "", {
-//     parentChunker: (text: string) => TextChunker.sentences(text),
-//     childChunker: (parentText: string) =>
-//       TextChunker.fixedCharacterLength(parentText, { chunkLength: 100, overlap: 15 })
-//   });
-
-//   const result = await Rag.standard(chunks, context)
-//     .selector(x => x.doc)
-//     .limit(12)
-//     .sortByIndex()
-//     .onlyUnique()
-//     .query(query);
-
-//   message.content = previewChunks(
-//     result.map(x => x.metadata.parent),
-//     2000
-//   );
-// };
-
-const advancedFilterMessageContent = async (message: ChatMessage, query: string, context: AgentContext, askLlm: (query: string | Prompt) => Promise<string>): Promise<void> => {
-  const chunks = TextChunker.parentDocRetrieval(message.content ?? "", {
-    parentChunker: (text: string) => TextChunker.sentences(text),
-    childChunker: (parentText: string) =>
-      TextChunker.fixedCharacterLength(parentText, { chunkLength: 100, overlap: 15 })
-  });
-
-  const result = await Rag.standard(chunks, context)
-    .selector(x => x.doc)
-    .limit(48)
-    .sortByIndex()
-    .onlyUnique()
-    .query(query);
-
-  const bigPreview = previewChunks(
-    result.map(x => x.metadata.parent),
-    8000
-  );
-
-  const prompt = new Prompt()
-  .block(bigPreview)
-  .line(`Goal:`)
-  .line(x => x.block(query))
-  .line(`
-    Consider the above text in respect to the desired goal.
-    Rewrite the text to better achieve the goal.
-    Filter out unecessary information. Do not add new information.
-    IMPORTANT: Respond only with the new text!`
-  ).toString();
-
-  console.log(prompt);
-
-  message.content = await askLlm(
-    prompt
-  );
-
-  console.log("CONTENT", message.content);
-};
 
 const buildDirectoryPreviewMsg = (workspace: Workspace): ChatMessage => {
   const files = workspace.readdirSync("./");
