0f535a4f8bba0c9d9656a7c46f506a1e03b58613
Cesar <cesarbrazon10@gmail.com>
2023-10-23 23:50:18 +0200

    chore: remove temporary character chunker

--- packages/agent-utils/src/chunking/MessageChunker.ts
+++ packages/agent-utils/src/chunking/MessageChunker.ts
@@ -1,6 +1,6 @@
 import { ChatMessage } from "../llm";
 import { Chunker } from "./Chunker";
-import { chunkByCharacters } from "./CharactersChunker";
+import { TextChunker } from "./TextChunker";
 
 export class MessageChunker implements Chunker<ChatMessage> {
   constructor(private readonly config: { maxChunkSize: number }) { }
@@ -10,10 +10,12 @@ export class MessageChunker implements Chunker<ChatMessage> {
   }
 
   chunk(message: ChatMessage): string[] {
-    return chunkByCharacters(
+    return TextChunker.fixedCharacterLength(
       message.content || "",
-      this.config.maxChunkSize,
-      0.08 * this.config.maxChunkSize
+      { 
+        chunkLength: this.config.maxChunkSize,
+        overlap: 0.08 * this.config.maxChunkSize
+      }
     ).map((chunk) => (JSON.stringify({
       ...message,
       content: chunk
