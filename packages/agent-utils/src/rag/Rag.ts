360ce505a80c050b18016c5b0612202d8d9d98d1
nerfZael <bogunovij@gmail.com>
2023-10-26 01:39:23 +0200

    agent rag is now a singleton

--- packages/agent-utils/src/rag/Rag.ts
+++ packages/agent-utils/src/rag/Rag.ts
@@ -1,10 +1,11 @@
 import { StandardRagBuilder } from "./StandardRagBuilder";
 import { AgentContext } from "../agent/AgentContext";
 import { TextChunker, TextRecombiner } from "../chunking";
+import { Workspace } from "../sys";
 
 export class Rag {
-  static standard<TItem = string>(context: AgentContext, collectionName?: string): StandardRagBuilder<TItem> {
-    return new StandardRagBuilder<TItem>(context, collectionName);
+  static standard<TItem = string>(context: AgentContext, collectionName?: string, workspace?: Workspace, items?: TItem[]): StandardRagBuilder<TItem> {
+    return new StandardRagBuilder<TItem>(context, collectionName, workspace, items);
   }
 
   static async filterWithSurroundingText(text: string, queryOrQueryVector: string | number[], context: AgentContext, opts: { tokenLimit: number, surroundingCharacters: number, chunkLength: number, overlap: number}): Promise<string> {
