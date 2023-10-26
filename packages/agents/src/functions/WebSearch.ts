9bd0d2edb2b3c746a45dfb0fb529c0437286d180
namesty <nestor09amesty@gmail.com>
2023-10-26 11:32:49 +0200

    Query description hotfix

--- packages/agents/src/functions/WebSearch.ts
+++ packages/agents/src/functions/WebSearch.ts
@@ -48,7 +48,7 @@ export class WebSearchFunction extends LlmAgentFunctionBase<WebSearchFuncParamet
         items: {
             type: "string"
         },
-        description: "Queries to search the web for in parallel",
+        description: "Queries with all details to search the web for in parallel",
       },
     },
     required: ["queries"],
