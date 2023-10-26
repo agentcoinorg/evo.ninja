74c3c3e4ff0f38f78360db783d922a04f5ce0637
dOrgJelli <jelli@dorg.tech>
2023-10-26 08:21:23 +0200

    feat: manually call agent functions on startup

--- packages/agents/src/scriptedAgents/Researcher/index.ts
+++ packages/agents/src/scriptedAgents/Researcher/index.ts
@@ -1,5 +1,5 @@
-import { Agent } from "../../Agent";
-import { AgentContext } from "@evo-ninja/agent-utils";
+import { Agent, GoalRunArgs } from "../../Agent";
+import { AgentContext, executeAgentFunction, agentFunctionBaseToAgentFunction, Chat } from "@evo-ninja/agent-utils";
 import { AgentConfig } from "../../AgentConfig";
 import { WebSearchFunction } from "../../functions/WebSearch";
 import { PlanWebResearchFunction } from "../../functions/PlanWebResearch";
@@ -10,6 +10,8 @@ import { ScrapeTableFunction } from "../../functions/ScrapeTable";
 import { ScrapeTextFunction } from "../../functions/ScrapeText";
 
 export class ResearcherAgent extends Agent {
+  private plan: PlanWebResearchFunction;
+
   constructor(context: AgentContext) {
     super(
       new AgentConfig(
@@ -26,5 +28,26 @@ export class ResearcherAgent extends Agent {
       ),
       context
     );
+    this.plan = new PlanWebResearchFunction(context.llm, context.chat.tokenizer);
+  }
+
+  public override async onFirstRun(args: GoalRunArgs, chat: Chat): Promise<void> {
+    // TODO: this is duplicated from basicFunctionCallLoop, should be generalized into a utility
+    const fn = agentFunctionBaseToAgentFunction(this)(this.plan);
+    const { result } = await executeAgentFunction([args, fn], JSON.stringify(args), this.context);
+
+    // Save large results as variables
+    for (const message of result.messages) {
+      if (message.role !== "function") {
+        continue;
+      }
+      const functionResult = message.content || "";
+      if (result.storeInVariable || this.context.variables.shouldSave(functionResult)) {
+        const varName = this.context.variables.save(this.plan.name, functionResult);
+        message.content = `\${${varName}}`;
+      }
+    }
+
+    result.messages.forEach(x => chat.temporary(x));
   }
 }
