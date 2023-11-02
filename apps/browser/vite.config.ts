9f06cbb671905e60a5c4048a38e9299cd40d0907
namesty <nestor09amesty@gmail.com>
2023-11-02 15:33:10 +0100

    Vite config updated

--- apps/browser/vite.config.ts
+++ apps/browser/vite.config.ts
@@ -1,8 +1,6 @@
+import react from "@vitejs/plugin-react"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()]
 })
