2ea467bbc260dcec2004b40c1b4880baca83506e
Cesar <cesarbrazon10@gmail.com>
2023-11-06 14:21:53 +0100

    chore: fix ci & remove unused attributes from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,6 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       parserOptions: {
         project: ["tsconfig.json", "./packages/**/tsconfig.json"],
-        ecmaVersion: 2020,
-        sourceType: "module"
       },
       plugins: [
         "eslint-plugin-import",
