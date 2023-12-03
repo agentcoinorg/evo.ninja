febfc2819ef26a1b5f7dc31a5e6ca061dc64dcc8
namesty <nestor09amesty@gmail.com>
2023-12-03 22:54:30 +0100

    Made NextAuth needed env vars not needed at build time

--- apps/browser/app/api/auth/[...nextauth]/route.ts
+++ apps/browser/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,11 @@
 import { getAuthOptions } from "@/lib/api/authOptions";
 import NextAuth from "next-auth";
+import { NextRequest } from "next/server";
 
-const handler = NextAuth(getAuthOptions());
-export { handler as GET, handler as POST };
+export function GET(req: NextRequest, res: any) {
+  return NextAuth(req, res, getAuthOptions())
+}
+
+export function POST(req: NextRequest, res: any) {
+  return NextAuth(req, res, getAuthOptions())
+}
