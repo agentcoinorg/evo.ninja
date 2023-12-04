14b85d875a3f293c78a8f7a68444e6b0de62a837
namesty <nestor09amesty@gmail.com>
2023-12-04 14:07:29 +0100

    OpenAI API key sessions

--- apps/browser/app/api/auth/[...nextauth]/route.ts
+++ apps/browser/app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,9 @@ import { getAuthOptions } from "@/lib/api/authOptions";
 import NextAuth from "next-auth";
 import { NextRequest } from "next/server";
 
-export function GET(req: NextRequest, res: any) {
-  return NextAuth(req, res, getAuthOptions())
+export async function GET(req: NextRequest, res: any) {
+  const isDefaultSigninPage = req.nextUrl.pathname.includes("signin")
+  return await NextAuth(req, res, getAuthOptions(isDefaultSigninPage));
 }
 
 export function POST(req: NextRequest, res: any) {
