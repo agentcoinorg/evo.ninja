fcc596b4fb496a3e10b0ee13207ca77422c14b16
Nestor Amesty <nestor09amesty@gmail.com>
2023-12-11 20:33:18 +0100

    Eliminated supabase from jotai store

--- apps/browser/lib/store.ts
+++ apps/browser/lib/store.ts
@@ -23,16 +23,3 @@ export const sidebarAtom = atom<boolean>(true)
 export const chatIdAtom = atom<string>("")
 export const proxyLlmAtom = atom<ProxyLlmApi | undefined>(undefined)
 export const proxyEmbeddingAtom = atom<ProxyEmbeddingApi | undefined>(undefined)
-
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw Error("Env missing NEXT_PUBLIC_SUPABASE_URL");
-}
-
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw Error("Env missing NEXT_PUBLIC_SUPABASE_ANON_KEY");
-}
-
-export const supabaseAtom = atom<SupabaseClient<Database>>(createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-))
