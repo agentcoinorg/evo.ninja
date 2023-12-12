15282b5cafac1d336e6253a6fb203079b48cec87
jSydorowicz21 <jonathan.sydorowicz@mykenzie.snhu.edu>
2023-12-12 14:29:56 -0500

    Added ReadDirectoryFunction to the relevant agents allowing them to read and understand directories within it's workspace

--- packages/agents/src/agents/Synthesizer/index.ts
+++ packages/agents/src/agents/Synthesizer/index.ts
@@ -1,18 +1,19 @@
 import { WriteFileFunction } from "../../functions/WriteFile";
 import { ReadFileFunction } from "../../functions/ReadFile";
+import { ReadDirectoryFunction } from "../../functions/ReadDirectory";
 import { AgentContext } from "@/agent-core";
 import { prompts } from "./prompts";
 import { Agent, AgentConfig } from "../utils";
 
 export class SynthesizerAgent extends Agent {
   constructor(context: AgentContext) {
-
     super(
       new AgentConfig(
         () => prompts,
         [
           new WriteFileFunction(context.scripts),
           new ReadFileFunction(context.scripts),
+          new ReadDirectoryFunction(context.scripts),
         ],
         context.scripts
       ),
