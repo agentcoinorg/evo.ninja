11579dde0fdd65741058aa10fdf46476fb2e7060
Colin Spence <udappia@gmail.com>
2023-12-13 14:26:22 -0700

    Add skeleton states for: Recent Chats, Workspace, Account

--- apps/browser/components/CurrentWorkspace.tsx
+++ apps/browser/components/CurrentWorkspace.tsx
@@ -7,10 +7,14 @@ import FileIcon from "./FileIcon";
 import { DownloadSimple, FilePlus } from "@phosphor-icons/react";
 import Button from "./Button";
 import { useUploadFiles } from "@/lib/hooks/useUploadFile";
+import { useAtom } from "jotai";
+import { uploadedFilesAtom } from "@/lib/store";
 
 interface UploadProps {
   userFiles: InMemoryFile[];
 }
+
+const loadedWorkspace = true;
 
 function CurrentWorkspace({ userFiles }: PropsWithChildren<UploadProps>) {
   const { getRootProps, getInputProps, isDragAccept, open } = useUploadFiles();
@@ -26,67 +30,77 @@ function CurrentWorkspace({ userFiles }: PropsWithChildren<UploadProps>) {
         <div className="text-xs uppercase tracking-widest text-zinc-500">
           Current Workspace
         </div>
-        <div className="flex items-center space-x-1">
-          <Button variant="icon" onClick={open}>
-            <FilePlus size={18} weight="bold" />
-          </Button>
-          <input {...getInputProps()} />
-          {userFiles.length !== 0 && (
-            <Button
-              variant="icon"
-              className="text-zinc-500 hover:text-cyan-500"
-              onClick={() => downloadFilesAsZip("workspace.zip", userFiles)}
-            >
-              <DownloadSimple size={18} weight="bold" />
+        {loadedWorkspace && (
+          <div className="flex items-center space-x-1">
+            <Button variant="icon" onClick={open}>
+              <FilePlus size={18} weight="bold" />
             </Button>
-          )}
-        </div>
-      </div>
-      <div className="relative h-full max-h-[24vh] overflow-y-auto">
-        {userFiles.length === 0 ? (
-          <div
-            className="mt-1 flex cursor-pointer flex-col items-center justify-center space-y-2 rounded-lg border-2 border-dashed border-zinc-500 p-7 text-center transition-colors duration-300 hover:border-cyan-500 hover:bg-zinc-950 hover:text-cyan-500"
-            onClick={open}
-          >
-            <FilePlus size={24} className="text-[currentColor]" />
-            <p className="leading-regular text-xs text-zinc-500">
-              You currently have no files in your workspace. Drop or click here
-              to add them.
-            </p>
+            <input {...getInputProps()} />
+            {userFiles.length !== 0 && (
+              <Button
+                variant="icon"
+                className="text-zinc-500 hover:text-cyan-500"
+                onClick={() => downloadFilesAsZip("workspace.zip", userFiles)}
+              >
+                <DownloadSimple size={18} weight="bold" />
+              </Button>
+            )}
           </div>
-        ) : (
-          <>
-            <div
-              {...getRootProps({
-                className: clsx(
-                  "dropzone group h-full space-y-1 overflow-y-auto rounded-lg border-2 border-solid border-zinc-900 p-[6px] transition-all duration-100 ease-in-out",
-                  {
-                    "cursor-pointer !border-dashed !border-cyan-500 !bg-zinc-950":
-                      isDragAccept,
-                  }
-                ),
-              })}
-            >
-              {userFiles.map((file, i) => {
-                return (
-                  <div
-                    key={i}
-                    className={clsx(
-                      "flex w-full cursor-pointer items-center space-x-2 rounded p-1 text-sm text-cyan-500 transition-colors duration-300",
-                      { "hover:bg-zinc-800 hover:text-white": !isDragAccept }
-                    )}
-                  >
-                    <FileIcon fileType={getFileType(file.path)} />
-                    <div className="w-full overflow-x-hidden text-ellipsis whitespace-nowrap">
-                      {file.path}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </>
         )}
       </div>
+      {loadedWorkspace ? (
+        <>
+          <div className="relative h-full max-h-[24vh] overflow-y-auto">
+            {userFiles.length === 0 ? (
+              <div
+                className="mt-1 flex cursor-pointer flex-col items-center justify-center space-y-2 rounded-lg border-2 border-dashed border-zinc-500 p-7 text-center transition-colors duration-300 hover:border-cyan-500 hover:bg-zinc-950 hover:text-cyan-500"
+                onClick={open}
+              >
+                <FilePlus size={24} className="text-[currentColor]" />
+                <p className="leading-regular text-xs text-zinc-500">
+                  You currently have no files in your workspace. Drop or click
+                  here to add them.
+                </p>
+              </div>
+            ) : (
+              <>
+                <div
+                  {...getRootProps({
+                    className: clsx(
+                      "dropzone group h-full space-y-1 overflow-y-auto rounded-lg border-2 border-solid border-zinc-900 p-[6px] transition-all duration-100 ease-in-out",
+                      {
+                        "cursor-pointer !border-dashed !border-cyan-500 !bg-zinc-950":
+                          isDragAccept,
+                      }
+                    ),
+                  })}
+                >
+                  {userFiles.map((file, i) => {
+                    return (
+                      <div
+                        key={i}
+                        className={clsx(
+                          "flex w-full cursor-pointer items-center space-x-2 rounded p-1 text-sm text-cyan-500 transition-colors duration-300",
+                          {
+                            "hover:bg-zinc-800 hover:text-white": !isDragAccept,
+                          }
+                        )}
+                      >
+                        <FileIcon fileType={getFileType(file.path)} />
+                        <div className="w-full overflow-x-hidden text-ellipsis whitespace-nowrap">
+                          {file.path}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="m-2 mt-1 h-[20vh] w-[calc(100%-1rem)] animate-pulse rounded-lg bg-zinc-700" />
+      )}
     </div>
   );
 }
--- apps/browser/components/Sidebar.tsx
+++ apps/browser/components/Sidebar.tsx
@@ -19,6 +19,9 @@ export interface SidebarProps {
   sidebarOpen: boolean;
 }
 
+const loadedAccount = true;
+const loadedChats = true;
+
 const Sidebar = ({
   userFiles,
   sidebarOpen,
@@ -79,11 +82,11 @@ const Sidebar = ({
         )}
       >
         <div
-          className="flex h-full animate-fade-in flex-col justify-between opacity-0"
+          className="animate-fade-in flex h-full flex-col justify-between opacity-0"
           style={{ animationDelay: sidebarOpen ? "150ms" : "0ms" }}
         >
           <div className="flex h-full flex-col justify-between">
-            <div className="space-y-6">
+            <div className={clsx({ "space-y-6": session?.user.email })}>
               <header>
                 <a href="/">
                   <Logo className="w-[162px] cursor-pointer p-4 transition-opacity hover:opacity-50" />
@@ -95,80 +98,98 @@ const Sidebar = ({
                     <div className="text-xs uppercase tracking-widest text-zinc-500">
                       Recent Chats
                     </div>
-                    <Button variant="icon" onClick={createNewChat}>
-                      <NotePencil size={18} weight="bold" />
-                    </Button>
-                  </div>
-                  <div className="h-full max-h-[30vh] space-y-0.5 overflow-y-auto">
-                    {chats && chats.length > 0 ? (
-                      <div className="px-2">
-                        {mappedChats?.map((chat, i) => (
-                          <div
-                            key={chat.id}
-                            data-id={chat.id}
-                            className="w-full cursor-pointer overflow-x-hidden text-ellipsis whitespace-nowrap rounded p-1 text-sm text-zinc-100 transition-colors duration-300 hover:bg-zinc-700 hover:text-white"
-                            onClick={() => handleChatClick(chat.id)}
-                          >
-                            {chat.name}
-                          </div>
-                        ))}
-                      </div>
-                    ) : (
-                      <div
-                        onClick={createNewChat}
-                        className=" mt-1 flex cursor-pointer flex-col items-center justify-center space-y-2 rounded-lg border-2 border-dashed border-zinc-500 p-7 text-center transition-colors duration-300 hover:border-cyan-500 hover:bg-zinc-950 hover:text-cyan-500"
-                      >
-                        <NotePencil size={24} className="text-[currentColor]" />
-                        <p className="leading-regular text-xs text-zinc-500">
-                          You currently have no chats.
-                        </p>
-                      </div>
+                    {loadedChats && (
+                      <Button variant="icon" onClick={createNewChat}>
+                        <NotePencil size={18} weight="bold" />
+                      </Button>
                     )}
                   </div>
+                  {loadedChats ? (
+                    <div className="h-full max-h-[30vh] space-y-0.5 overflow-y-auto">
+                      {chats && chats.length > 0 ? (
+                        <div className="px-2">
+                          {mappedChats?.map((chat, i) => (
+                            <div
+                              key={chat.id}
+                              data-id={chat.id}
+                              className="w-full cursor-pointer overflow-x-hidden text-ellipsis whitespace-nowrap rounded p-1 text-sm text-zinc-100 transition-colors duration-300 hover:bg-zinc-700 hover:text-white"
+                              onClick={() => handleChatClick(chat.id)}
+                            >
+                              {chat.name}
+                            </div>
+                          ))}
+                        </div>
+                      ) : (
+                        <div
+                          onClick={createNewChat}
+                          className=" mt-1 flex cursor-pointer flex-col items-center justify-center space-y-2 rounded-lg border-2 border-dashed border-zinc-500 p-7 text-center transition-colors duration-300 hover:border-cyan-500 hover:bg-zinc-950 hover:text-cyan-500"
+                        >
+                          <NotePencil
+                            size={24}
+                            className="text-[currentColor]"
+                          />
+                          <p className="leading-regular text-xs text-zinc-500">
+                            You currently have no chats.
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  ) : (
+                    <div className="mx-2 h-[20vh] w-[calc(100%-1rem)] animate-pulse rounded-lg bg-zinc-700" />
+                  )}
                 </div>
               )}
               <CurrentWorkspace userFiles={userFiles} />
             </div>
-            <div className="relative flex w-full items-center justify-between p-4">
-              <DropdownAccount ref={dropdownRef} dropdownOpen={dropdownOpen} />
-              <div
-                className={clsx(
-                  "inline-flex w-full -translate-x-2 transform cursor-pointer items-center space-x-2 rounded-lg p-2 transition-colors hover:bg-zinc-800",
-                  { "bg-zinc-800": dropdownOpen }
-                )}
-                onClick={() => setDropdownOpen(!dropdownOpen)}
-              >
-                {session?.user.email ? (
-                  <>
-                    <div className="flex items-center space-x-2">
-                      {session.user.image ? (
-                        <img
-                          src={session.user.image}
-                          className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600"
+            <div className="relative flex w-full items-center justify-between space-x-2 p-4">
+              {loadedAccount ? (
+                <>
+                  <DropdownAccount
+                    ref={dropdownRef}
+                    dropdownOpen={dropdownOpen}
+                  />
+                  <div
+                    className={clsx(
+                      "inline-flex w-full -translate-x-2 transform cursor-pointer items-center space-x-2 rounded-lg p-2 transition-colors hover:bg-zinc-800",
+                      { "bg-zinc-800": dropdownOpen }
+                    )}
+                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                  >
+                    {session?.user.email ? (
+                      <>
+                        <div className="flex items-center space-x-2">
+                          {session.user.image ? (
+                            <img
+                              src={session.user.image}
+                              className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600"
+                            />
+                          ) : (
+                            <div className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600" />
+                          )}
+                          <div className="w-full space-y-1 overflow-x-hidden">
+                            <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold leading-none">
+                              {session.user.name}
+                            </div>
+                            <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-[11px] leading-none text-gray-400">
+                              {session.user.email}
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <AvatarBlockie
+                          size={32}
+                          address={"0x7cB57B5A97eAbe94205C07890BE4c1aD31E486A8"}
                         />
-                      ) : (
-                        <div className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600" />
-                      )}
-                      <div className="w-full space-y-1 overflow-x-hidden">
-                        <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold leading-none">
-                          {session.user.name}
-                        </div>
-                        <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-[11px] leading-none text-gray-400">
-                          {session.user.email}
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <AvatarBlockie
-                      size={32}
-                      address={"0x7cB57B5A97eAbe94205C07890BE4c1aD31E486A8"}
-                    />
-                    <div className="text-white">Guest</div>
-                  </>
-                )}
-              </div>
+                        <div className="text-white">Guest</div>
+                      </>
+                    )}
+                  </div>
+                </>
+              ) : (
+                <div className="h-12 w-full animate-pulse rounded-lg bg-zinc-700" />
+              )}
               <div className="flex items-center space-x-1 text-lg text-white">
                 <a
                   href="https://discord.gg/r3rwh69cCa"
