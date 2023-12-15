7478ec35d23734e0aea4d236ba63889b49787329
Colin Spence <udappia@gmail.com>
2023-12-15 09:48:13 -0700

    removes redundant sign in data in welcome modal 2nd state

--- apps/browser/components/modals/WelcomeModal.tsx
+++ apps/browser/components/modals/WelcomeModal.tsx
@@ -23,78 +23,50 @@ export default function WelcomeModal(props: WelcomeModalProps) {
   return (
     <>
       <Modal isOpen={isOpen} title="Welcome to Evo Ninja" onClose={onClose}>
-        <div className="border-b-2 border-zinc-700 pb-8 text-center">
-          Evo is an agent that can do many things. This is a technical preview,
-          feedback and questions are appreciated!
-        </div>
         {session?.user?.email ? (
           <div className="space-y-6">
             <div className="border-b-2 border-zinc-700 pb-8 text-center">
               You're signed in!
             </div>
-            <div className="flex items-center justify-between rounded-lg bg-zinc-800 p-4">
-              <div className="flex items-center space-x-2">
-                {session.user.image ? (
-                  <img
-                    src={session?.user.image}
-                    className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600"
-                  />
-                ) : (
-                  <div className="h-8 w-8 min-w-[2rem] rounded-full bg-cyan-600" />
-                )}
-                <div className="space-y-1">
-                  <div className="text-sm font-semibold leading-none">
-                    {session.user.name}
-                  </div>
-                  <div className="text-xs leading-none text-gray-400 underline">
-                    {session.user.email}
-                  </div>
-                </div>
-              </div>
-              <div className="space-x-2">
+          </div>
+        ) : (
+          <>
+            <div className="border-b-2 border-zinc-700 pb-8 text-center">
+              Evo is an agent that can do many things. This is a technical
+              preview, feedback and questions are appreciated!
+            </div>
+            <div className="space-y-6 border-b-2 border-zinc-700 pb-8">
+              <p>Sign in below to save your sessions</p>
+              <div className="space-y-2">
                 <Button
-                  className="!px-4"
+                  className="w-full"
                   hierarchy="secondary"
-                  onClick={() => signOut()}
+                  onClick={() => signIn("github")}
                 >
-                  <SignOut color="currentColor" size={16} />
-                  <div>Sign Out</div>
+                  <Image
+                    alt="Sign in with Github"
+                    width={20}
+                    height={20}
+                    src="/github-logo.svg"
+                  />
+                  <div>Sign in with Github</div>
+                </Button>
+                <Button
+                  className="w-full"
+                  hierarchy="secondary"
+                  onClick={() => signIn("google")}
+                >
+                  <Image
+                    alt="Sign in with Google"
+                    width={20}
+                    height={20}
+                    src="/google-logo.svg"
+                  />
+                  <div>Sign in with Google</div>
                 </Button>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="space-y-6 border-b-2 border-zinc-700 pb-8">
-            <p>Sign in below to save your sessions</p>
-            <div className="space-y-2">
-              <Button
-                className="w-full"
-                hierarchy="secondary"
-                onClick={() => signIn("github")}
-              >
-                <Image
-                  alt="Sign in with Github"
-                  width={20}
-                  height={20}
-                  src="/github-logo.svg"
-                />
-                <div>Sign in with Github</div>
-              </Button>
-              <Button
-                className="w-full"
-                hierarchy="secondary"
-                onClick={() => signIn("google")}
-              >
-                <Image
-                  alt="Sign in with Google"
-                  width={20}
-                  height={20}
-                  src="/google-logo.svg"
-                />
-                <div>Sign in with Google</div>
-              </Button>
-            </div>
-          </div>
+          </>
         )}
         <AccountConfig
           setApiKey={setApiKey}
