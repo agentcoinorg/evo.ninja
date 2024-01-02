fbfc8857b2df9b29ca4024f526759f4a35f81a92
Nestor Amesty <nestor09amesty@gmail.com>
2024-01-02 18:35:25 +0100

    Changed implementation of grading function

--- evo_researcher/functions/grade_info.py
+++ evo_researcher/functions/grade_info.py
@@ -1,73 +1,78 @@
-import json
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.output_parsers import PydanticOutputParser
-from langchain_core.runnables import RunnableParallel
 from langchain_core.pydantic_v1 import BaseModel
 
-class GradeDetails(BaseModel):
-    explanation: str
-    grade: int
-
 class InfoScores(BaseModel):
-    relevance: GradeDetails
-    accuracy: GradeDetails
-    completeness: GradeDetails
-    timeliness: GradeDetails
-    objectivity: GradeDetails
-    usefulness: GradeDetails
-    final_grade: GradeDetails
+    credibility: int
+    bias: int
+    accuracy: int
+    currency: int
+    relevance: int
+    significance: int
+    
+grading_planning_prompt_template = """
+You are a professional information evaluator. Your goal is to 
+evaluate research for making an informed prediction to answer the question: {question}.
+
+Limit yourself to the following criteria:
+
+* Credibility: Assess the source's credibility based on the author's credentials, logical arguments, and documentation backed by scholarly literature​​.
+* Bias: Examine if the source has a hidden agenda or dismisses other viewpoints​​.
+* Accuracy: Check for supporting documentation for presented facts and contradictions with other reliable sources​​.
+* Currency: Ensure the information is current and up-to-date, particularly important for rapidly evolving topics​​.
+* Relevance: The information should be relevant to the specific topic of interest​​.
+* Significance: Evaluate if the information is significant and not trivial or too general​
+
+How would you approach grading the information for each of these criteria?
+Let's think about it step by step.
+"""
 
 
 grading_prompt_template = """
-    You are a professional information evaluator. Your goal is to grade the information
-    that is given to you.
-    
-    The information you will be given is research that will be used to make an informed
-    prediction on the question: {question}.
-    
-    Consider the following criteria:
-    
-    - relevance: How relevant is the information to the question?
-    - accuracy: How accurate is the information?
-    - completeness: How complete is the information?
-    - timeliness: How timely is the information?
-    - objectivity: How objective is the information?
-    - usefulness: How useful is the information?
-    
-    Please grade the information on an integer scale of 1-100 for each criteria.
-    Take it step by step and provide a detailed explanation for each grade.
-    
-    Finally provide a final general grade for the information as a whole.
-    
-    Respond strictly in a single JSON formatted string, with the following form:
-    
-    {{ "relevance": {{ "explanation": "...", "grade": "..." }}, ..., "final_grade": {{ ... }} }}
-    
-    Information:
-    
-    {info}
-    """
-    
-aggregate_grading_prompt_template = """
-    I have generated the following results to grade information on the question: {question}.
-    
-    Select the most consistent result based on majority consensus.
-    Respond strictly in a single JSON formatted string, with the following form:
-    
-    {{ "relevance": {{ "explanation": "...", "grade": "..." }}, ..., "final_grade": {{ ... }} }}
-    
-    Results:
-    
-    {results}
-    """
+You are a professional information evaluator. Your goal is to 
+evaluate research for making an informed prediction to answer the question: {question}.
 
-def grade_info(info: str, question: str, consistency_iterations: int = 3) -> InfoScores:
-    
+Here is the plan you came up with to evaluate the information:
+
+{plan}
+
+Limit yourself to the criteria in the plan.
+
+Now you will be given a piece of information to evaluate, according to the plan.
+Provide a single numeric score for each criteria, from 0 to 100, where 0 is the worst and 100 is the best.
+Provide a step by step justification for each score.
+
+Information:
+
+{information}
+"""
+
+grading_format_prompt_template = """
+You will be given an evaluation of information you graded. It contains scores for different
+evaluation criteria. Extract the score for each criteria and return them in the following
+format:
+
+{{ "one_of_the_criteria": xx, ... }}
+
+Where xx is a number. Only respond with a single JSON. The keys should be lowercase and snake_case.
+
+{report}
+"""
+
+def grade_info(question: str, information: str) -> InfoScores:
     grading_prompt = ChatPromptTemplate.from_template(template=grading_prompt_template)
+    planning_prompt = ChatPromptTemplate.from_template(template=grading_planning_prompt_template)
+    formatting_prompt = ChatPromptTemplate.from_template(template=grading_format_prompt_template)
     
     llm = ChatOpenAI(model="gpt-4-1106-preview")
+    
+    planning_chain = (
+        planning_prompt |
+        llm |
+        StrOutputParser()
+    )
     
     grading_chain = (
         grading_prompt |
@@ -75,27 +80,30 @@ def grade_info(info: str, question: str, consistency_iterations: int = 3) -> Inf
         StrOutputParser()
     )
     
-    parallel_map = {f"grading_{i}": grading_chain for i in range(consistency_iterations)}
-    
-    parallel_chain = RunnableParallel(parallel_map)
-    grading_results = parallel_chain.invoke({
-        "info": info,
-        "question": question
-    })
-    
-    grading_results = list(grading_results.values())
-
-    aggregate_grading_prompt = ChatPromptTemplate.from_template(template=aggregate_grading_prompt_template)
-
-    aggregate_grading_chain = (
-        aggregate_grading_prompt |
+    formatting_chain = (
+        formatting_prompt |
         llm |
         PydanticOutputParser(pydantic_object=InfoScores)
     )
     
-    response = aggregate_grading_chain.invoke({
-        "question": question,
-        "results": json.dumps(grading_results)
+    plan = planning_chain.invoke({
+        "question": question
     })
     
-    return response
+    print(plan)
+    
+    report = grading_chain.invoke({
+        "question": question,
+        "plan": plan,
+        "information": information
+    })
+    
+    print(report)
+    
+    grades = formatting_chain.invoke({
+        "report": report
+    })
+    
+    print(grades)
+    
+    return grades
