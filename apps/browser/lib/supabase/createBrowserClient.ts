26fdeb1494efc89adb85bc9de7a4533ebd5b431c
dOrgJelli <jelli@dorg.tech>
2024-01-23 16:18:03 +0100

    chore: add back supbase token

--- apps/browser/lib/supabase/createBrowserClient.ts
+++ apps/browser/lib/supabase/createBrowserClient.ts
@@ -1,10 +1,17 @@
 import { Database } from "./dbTypes";
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createSupabaseBrowserClient = () => {
+export const createSupabaseBrowserClient = (supabaseAccessToken: string) => {
   const client = createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
+      cookies: {}
+    }
   );
 
   return client;
